96c38feb999b83587a90be9ef00413c5ee331522
Татьяна <tnosinko@gmail.com>
2024-10-18 10:00:33 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,8 @@
+first_point="12"
+print ("first_point","= 12")
+second_point="1.5"
+print ("second_point","= 1.5")
+course_name= "Python"
+print ("course_ name","= Python")
+print ("four_point",(12-1.5))
+print ('Course:Python, total tasks:12, hours spent:1.5, average execution time:0,125 hour')
